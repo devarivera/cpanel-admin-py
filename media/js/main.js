0978cfcc3b112ff451f771fc66b943e24e783e71
devarivera <52551726+devarivera@users.noreply.github.com>
2019-12-06 13:03:58 -0500

    add new label called quizz

--- media/js/main.js
+++ media/js/main.js
@@ -26,8 +26,8 @@ methods:{
       }
 
     },
-    btnediItem:async function(id,has_trial_coupon,mkting_client,ismarket_listing){
-      var checked_has='',checked_mkting_client='',checked_ismarket_listing='';
+    btnediItem:async function(id,has_trial_coupon,mkting_client,ismarket_listing,user_show_quizz){
+      var checked_has='',checked_mkting_client='',checked_ismarket_listing='',checked_user_show_quizz='';
       if (has_trial_coupon=='1') {
         checked_has='checked';
       }
@@ -40,9 +40,14 @@ methods:{
         checked_ismarket_listing='checked';
       }
 
+      if (user_show_quizz=='1') {
+        checked_user_show_quizz='checked';
+      }
+      
+
         await Swal.fire({
         title: 'Setting Account Cpanel',
-        html:'<div class="row"><label class="col-sm-7 col-form-label">Modal Pay Active?</label><div class="col-sm-2"><input id="has_trial_coupon" type="checkbox" '+checked_has+' /></div></div><div class="row"><label class="col-sm-7 col-form-label">is marketing client ?</label><div class="col-sm-2"><input id="mkting_client" type="checkbox" '+checked_mkting_client+' /></div></div><div class="row"><label class="col-sm-7 col-form-label">Off Market Listing Active ?</label><div class="col-sm-2"><input id="ismarket_listing" type="checkbox" '+checked_ismarket_listing+' /></div></div>',        
+        html:'<div class="row"><label class="col-sm-7 col-form-label">Modal Pay Active?</label><div class="col-sm-2"><input id="has_trial_coupon" type="checkbox" '+checked_has+' /></div></div><div class="row"><label class="col-sm-7 col-form-label">is marketing client ?</label><div class="col-sm-2"><input id="mkting_client" type="checkbox" '+checked_mkting_client+' /></div></div><div class="row"><label class="col-sm-7 col-form-label">Off Market Listing Active ?</label><div class="col-sm-2"><input id="ismarket_listing" type="checkbox" '+checked_ismarket_listing+' /></div></div> <div class="row"><label class="col-sm-7 col-form-label">QUizz is Active ?</label><div class="col-sm-2"><input id="isuser_show_quizz" type="checkbox" '+checked_user_show_quizz+' /></div></div>',
         focusConfirm: false,
         showCancelButton: true,                         
         }).then((result) => {
@@ -50,8 +55,9 @@ methods:{
               has_trial_coupon = document.getElementById('has_trial_coupon').checked,
               mkting_client = document.getElementById('mkting_client').checked,
               ismarket_listing = document.getElementById('ismarket_listing').checked,          
+              user_show_quizz = document.getElementById('isuser_show_quizz').checked,
             
-            this.ediItem(id,has_trial_coupon,mkting_client,ismarket_listing);
+            this.ediItem(id,has_trial_coupon,mkting_client,ismarket_listing,user_show_quizz);
             Swal.fire(
               '',
               'Registration updated successfully.',
@@ -69,9 +75,9 @@ methods:{
         });
     },    
 
-    ediItem:function(id,has_trial_coupon,mkting_client,ismarket_listing){       
+    ediItem:function(id,has_trial_coupon,mkting_client,ismarket_listing,user_show_quizz){
       console.log(has_trial_coupon,mkting_client,ismarket_listing);
-      var vtrial='0',vismarket='0',vmkting='0';
+      var vtrial='0',vismarket='0',vmkting='0',isquizz='0';
       var data = new FormData();
     
       if (has_trial_coupon=='on' || has_trial_coupon!=false ) 
@@ -83,10 +89,15 @@ methods:{
       if (ismarket_listing=='on' || ismarket_listing!=false) 
         vismarket='1';              
 
+      if (user_show_quizz=='on' || user_show_quizz!=false) 
+        isquizz='1';
+
+
       data.append("id", id );
       data.append("has_trial_coupon", vtrial );
       data.append("mkting_client", vmkting );
       data.append("ismarket_listing", vismarket );
+      data.append("user_show_quizz", isquizz );
 
       axios.post(idxUrl.generate, data ).then(response =>{
         this.listData();           
